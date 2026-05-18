d0222393d2960dcbcdf36cb2d12f9ca696585324
Daniel Cruz <20253ds058@utez.edu.mx>
2026-05-18 08:36:51 -0600

    [NEW] creacion de clase persona

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Persona {
+
+    private String nombre;
+    private String apellidos;
+     private int edad;
+
+
+     public Persona (String nombre, String apellidos, int edad) {
+
+         this.nombre = nombre;
+         this.apellidos = apellidos;
+         this.edad = edad;
+
+     }
+
+}
