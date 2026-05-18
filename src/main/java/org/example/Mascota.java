ad7ed60067c11b99448832f7f3063a556ce7474b
Daniel Cruz <20253ds058@utez.edu.mx>
2026-05-18 08:45:14 -0600

    [NEW] creacion de clase mascota

---
+++ src/main/java/org/example/Mascota.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Mascota {
+
+    private String nombre;
+    private String raza;
+    private int edad;
+
+    public Mascota(String nombre, String raza, int edad) {
+
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+
+    }
+}
